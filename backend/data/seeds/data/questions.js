926218fd7bbad3be9a7c41f79748859845ddc835
BrandCam <BrandCam>
2019-02-18 19:54:51 -0600

    added seeds for surveys, questions, questionAnswers, need bug fixing. also added endpoint to see data w/o a token

---
+++ backend/data/seeds/data/questions.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    question: 'what is this anyways?',
+    // survey_id: 1,
+  },
+  {
+    question: 'why?',
+    // survey_id: 1,
+  },
+  {
+    question: 'testy?',
+    // survey_id: 1,
+  },
+  {
+    question: 'woorrrkkk?',
+    // survey_id: 2,
+  },
+  {
+    question: 'plz?',
+    // survey_id: 4,
+  },
+];
