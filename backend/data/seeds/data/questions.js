aaf4af269757a687c47f70121049ac5650ece931
David Freitag <freitagdavid@outlook.com>
2019-02-19 19:18:10 -0500

    Seeds should be working now

--- backend/data/seeds/data/questions.js
+++ backend/data/seeds/data/questions.js
@@ -1,22 +1,22 @@
 module.exports = [
   {
     question: 'what is this anyways?',
-    // survey_id: 1,
+    survey_id: 1,
   },
   {
     question: 'why?',
-    // survey_id: 1,
+    survey_id: 1,
   },
   {
     question: 'testy?',
-    // survey_id: 1,
+    survey_id: 2,
   },
   {
     question: 'woorrrkkk?',
-    // survey_id: 2,
+    survey_id: 2,
   },
   {
     question: 'plz?',
-    // survey_id: 4,
+    survey_id: 4,
   },
 ];
