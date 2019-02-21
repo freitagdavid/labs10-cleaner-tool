26dc48b32a9608bc9ee64a7f5c307f1e186e0230
kelly <kelfro@fastmail.com>
2019-02-21 09:47:05 -0800

    Revert "Merge branch 'master' into labs10-post-survey"
    This reverts commit 42d716faa82e9175441b8ddb68ca1b774500d119, reversing
    changes made to 2f7f6cf8e95d1d23ea54c291338cbfe54f3332a2.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,29 +14,22 @@ import * as payments from './controller/payments';
 import * as stays from './controller/stays';
 import * as connect from './controller/connect';
 import * as assistants from './controller/assistants';
-import * as surveys from './controller/surveys';
 import path from 'path';
 
-import {
-  getSurveyResponse,
-  getSurvey,
-  getQuestionsAnswers,
-} from './models/surveys';
+import { getSurveyQuestions, getSurvey } from './models/surveys'
 
 import db from '../data/dbConfig';
 
 export const server = express();
 setGeneralMiddleware(server);
 
-server.get('/data', async (req, res) => {
-  try {
-    const data = await db('user');
-    console.log(data);
-    res.json(data);
-  } catch (e) {
-    res.json(e);
-  }
-});
+server.get('/data', async(req,res)=>{
+  try{
+  const users = await db('user')
+  console.log(users)
+  res.json(users);
+  }catch(e){res.json(e)}
+})
 server.use(express.static(path.resolve(path.join(__dirname, '../public'))));
 server.get('/', (__, res) => res.sendFile('index.html'));
 
@@ -67,35 +60,19 @@ server.get('/', (__, res) => res.sendFile('index.html'));
         res.json({ survey });   
     }catch(e){
       res.json(e), console.log(e)
-
-  server.get('/surveyResponses/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    console.log(id);
-    const survey = await getSurvey(id);
-    console.log(survey);
-    if (survey) {
-      const questionsAnswers = await getQuestionsAnswers(id);
-      res.json({ survey, questionsAnswers });
     }
-  } catch (e) {
-    res.json(e), console.log(e);
-  }
-});
+  });
 
-// server.get('/surveysquestions/:id', async(req,res)=>{
-//   try{
-//     const { id } = req.params
-//     const survey = await getSurveyResponse(id)
-//       res.json({ survey });
-//   }catch(e){
-//     res.json(e), console.log(e)
-//   }
-// });
-
-server.get('/questions', async (req, res) => {
-  try {
-    const data = await db('questions');
+  server.get('/questions', async(req,res)=>{
+    try{
+    const data = await db('questions')
+    res.json(data);
+    }catch(e){res.json(e)}
+  })
+  
+  server.get('/questionanswers', async(req,res)=>{
+    try{
+    const data = await db('questionAnswers')
     res.json(data);
     }catch(e){res.json(e)}
   })
@@ -126,17 +103,6 @@ server.delete('/surveys/:id', async (req, res) => {
   }
   catch (e) { res.json(e) }
 })   
-
-server.get('/questionanswers/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const questionAnswers = await getQuestionsAnswers(id);
-    res.json({ questionAnswers });
-  } catch (e) {
-    res.json(e);
-  }
-});
-
 server
   .route('/users')
   .get(verifyToken, users.get)
@@ -145,6 +111,7 @@ server
 // Authentication Middleware for *all* routes after this line
 server.use(verifyToken);
 
+  
 server
   .route('/users/:id')
   .get(users.get)
@@ -191,7 +158,6 @@ server
   .route('/items')
   .get(items.get)
   .post(items.post);
-
 server
   .route('/items/:id')
   .get(items.get)
@@ -220,11 +186,10 @@ server
   .get(stays.get)
   .put(stays.put);
 
-// Question mark makes the parameter optional
+  //dev endpoints 
 
-server.route('/surveys/:id?').get(surveys.get);
 
-// dev endpoints
+
 
 const options = {
   filePath: '../uploads',
