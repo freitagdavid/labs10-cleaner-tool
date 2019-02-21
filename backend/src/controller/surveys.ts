5f648ad721bbb11ae47aaae03aca9ccb3e76b8c9
David Freitag <freitagdavid@outlook.com>
2019-02-20 23:26:04 -0500

    backend/src/controller/surveys: create and working for by id

---
+++ backend/src/controller/surveys.ts
@@ -0,0 +1,25 @@
+import { getSurvey } from '../models/surveys';
+import { Request, Response, NextFunction } from 'express';
+import { RequestMock, ResponseMock } from '../../__tests__/helpers';
+import { QueryBuilder } from 'knex';
+
+type NextFunctionMock = (a: any) => any;
+type Requests = Request | RequestMock;
+type Responses = Response | ResponseMock;
+type Nexts = NextFunction | NextFunctionMock;
+let get: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+get = async (req, res, next) => {
+  const { id } = req.params;
+  if (id) {
+    try {
+      const surveys: QueryBuilder = await getSurvey(id);
+      res.status(200).json(surveys);
+    } catch (e) {
+      e.statusCode = e.statusCode || 400;
+      next(e);
+    }
+  }
+};
+
+export { get };
