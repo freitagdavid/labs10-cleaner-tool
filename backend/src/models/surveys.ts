326166f260536094e2235e642b3349cef1ea6122
David Freitag <freitagdavid@outlook.com>
2019-02-21 14:53:08 -0500

    Integrated our most recent changes

--- backend/src/models/surveys.ts
+++ backend/src/models/surveys.ts
@@ -1,45 +1,82 @@
 import { QueryBuilder } from 'knex';
 import db from '../../data/dbConfig';
 
-
 interface Surveys {
-    id?: number,
-    name: string,
-    isGuest: boolean
-    query: any
+  id: number;
+  name: string;
+  isGuest: boolean;
 }
 
-export function getSurveys(): QueryBuilder {
-    return db('surveys')
-}
+let getSurvey: (id: string) => QueryBuilder;
+let getAllSurveys: () => QueryBuilder;
+let getSurveyQuestions: (id: string) => QueryBuilder;
+let getSurveyResponse: (id: number) => QueryBuilder;
+let getQuestionsAnswers: (id: number) => QueryBuilder;
+// Boy this one was a bit of a stretch
+let filterByField: (
+  field: string,
+  fieldValue: string,
+) => (query: QueryBuilder) => QueryBuilder;
 
-export function getSurvey(id: number ): QueryBuilder {
-    return  db('surveys').where({ id }).first()
-}
+/* Don't know why but I had to protect this in a function before it would work
+  right otherwise it was returning a different sql statement every run */
 
-export function getSurveyResponse(id: number): QueryBuilder {
-    return db('surveys')
-        .join('questions', 'questions.survey_id', '=', 'surveys.id')
-        .join('questionAnswers', 'questionAnswers.question_id', '=', 'questions.id')
-        .select('surveys.name', 'questions.question', 'questionAnswers.answer')
-        .where({ survey_id: id, question_id: id })
-}
+const baseQuery = () => db('surveys');
 
-export function getAllQuestionsAnswers(): QueryBuilder {
-    return db('questions')
-        .join('questionAnswers', 'questionAnswers.question_id', '=', 'questions.id')
-        .select('questions.question', 'questionAnswers.answer')
-}
+filterByField = (field, fieldValue) => {
+  return (query) => {
+    return query.where(field, '=', fieldValue);
+  };
+};
 
-export function getQuestionsAnswers(id: number): QueryBuilder {
-    return db('questions')
-        .join('questionAnswers', 'questionAnswers.question_id', '=', 'questions.id')
-        .select('questions.question', 'questionAnswers.answer')
-        .where({ question_id: id})
-}
+getSurvey = (id) => {
+  const filteredById = filterByField('id', id);
+  return filteredById(baseQuery());
+};
 
+getAllSurveys = () => {
+  return baseQuery();
+};
 
+getSurveyByHouse = (id) => {};
 
+getSurveyQuestions = (id) => {
+  return baseQuery()
+    .join('questions', 'questions.survey_id', '=', 'survey.id')
+    .select(
+      'survey.name',
+      'question.name',
+      'survey.isGuest',
+      'question.isGuest',
+    )
+    .where({ survey_id: id });
+};
 
+getQuestionsAnswers = (id) => {
+  return db('questions')
+    .join('questionAnswers', 'questionAnswers.question_id', '=', 'questions.id')
+    .select('questions.question', 'questionAnswers.answer')
+    .where({ question_id: id });
+};
 
+getSurveyResponse = (id) => {
+  return baseQuery()
+    .join('questions', 'questions.survey_id', '=', 'survey.id')
+    .join('questionAnswers', 'questionAnswers.question_id', '=', 'questions.id')
+    .select(
+      'survey.name',
+      'questions.question',
+      'survey.isGuest',
+      'questions.question',
+      'questionAnswers.answer',
+    )
+    .where({ survey_id: id, question_id: id });
+};
 
+export {
+  getSurvey,
+  getAllSurveys,
+  getSurveyQuestions,
+  getSurveyResponse,
+  getQuestionsAnswers,
+};
