3fab8e494f6c481df512a23ce0fce5b8d3645723
David Freitag <freitagdavid@outlook.com>
2019-02-20 23:22:53 -0500

    backend/src/models/surveys: Survey by id and all surveys model working

--- backend/src/models/surveys.ts
+++ backend/src/models/surveys.ts
@@ -1,27 +1,35 @@
 import { QueryBuilder } from 'knex';
 import db from '../../data/dbConfig';
 
-
 interface Surveys {
-    id: number,
-    name: string,
-    isGuest: boolean
+  id: number;
+  name: string;
+  isGuest: boolean;
 }
+let getSurvey: (id: number) => QueryBuilder;
+let getAllSurveys: () => QueryBuilder;
+let getSurveyQuestions: (id: number) => QueryBuilder;
 
-export function getSurvey(id: number ): QueryBuilder {
-    return db('survey')
-}
+const baseQuery = db('surveys');
 
+getSurvey = (id) => {
+  return baseQuery.where({ id });
+};
 
-export function getSurveyQuestions(id: number): QueryBuilder {
-    return db('survey')
-        .join('questions', 'questions.survey_id', '=', 'survey.id')
-        .select('survey.name', 'question.name', 'survey.isGuest', 'question.isGuest')
-        .where({ survey_id: id })
-}
+getAllSurveys = () => {
+  return baseQuery;
+};
 
+getSurveyQuestions = (id) => {
+  return baseQuery
+    .join('questions', 'questions.survey_id', '=', 'survey.id')
+    .select(
+      'survey.name',
+      'question.name',
+      'survey.isGuest',
+      'question.isGuest',
+    )
+    .where({ survey_id: id });
+};
 
-
-
-
-
+export { getSurvey, getAllSurveys, getSurveyQuestions };
