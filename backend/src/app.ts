58c427bb77e9a38535815e3963962eada1241fa1
kelly <kelfro@fastmail.com>
2019-02-21 10:25:03 -0800

    Revert "Merge branch 'master' into labs10_survey_model"
    This reverts commit 0d8550946a0b64bfd4cb506d12f54b9c5f4089f0, reversing
    changes made to 5b5b3eb261b5a49175be6230a41558bfe6afdb84.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,9 +14,10 @@ import * as payments from './controller/payments';
 import * as stays from './controller/stays';
 import * as connect from './controller/connect';
 import * as assistants from './controller/assistants';
+import * as surveys from './controller/surveys';
 import path from 'path';
-
 import { getSurveys, getSurvey, getSurveyResponse, getAllQuestionsAnswers, getQuestionsAnswers } from './models/surveys'
+
 import db from '../data/dbConfig';
 
 export const server = express();
@@ -123,6 +124,7 @@ server
   .route('/items')
   .get(items.get)
   .post(items.post);
+
 server
   .route('/items/:id')
   .get(items.get)
@@ -151,10 +153,11 @@ server
   .get(stays.get)
   .put(stays.put);
 
-  //dev endpoints 
+// Question mark makes the parameter optional
 
+server.route('/surveys/:id?').get(surveys.get);
 
-
+// dev endpoints
 
 const options = {
   filePath: '../uploads',
--- backend/src/models/surveys.ts
+++ backend/src/models/surveys.ts
@@ -28,21 +28,54 @@ export function getAllQuestionsAnswers(): QueryBuilder {
     return db('questions')
         .join('questionAnswers', 'questionAnswers.question_id', '=', 'questions.id')
         .select('questions.question', 'questionAnswers.answer')
-
-export function getSurvey(id: number ): QueryBuilder {
-    return db('survey')
 }
 
-
-export function getSurveyQuestions(id: number): QueryBuilder {
-    return db('survey')
-        .join('questions', 'questions.survey_id', '=', 'survey.id')
-        .select('survey.name', 'question.name', 'survey.isGuest', 'question.isGuest')
-        .where({ survey_id: id })
+export function getQuestionsAnswers(id: number): QueryBuilder {
+  return db('questions')
+    .join('questionAnswers', 'questionAnswers.question_id', '=', 'questions.id')
+    .select('questions.question', 'questionAnswers.answer')
+    .where({ question_id: id });
 }
 
+let getSurvey: (id: string) => QueryBuilder;
+let getAllSurveys: () => QueryBuilder;
+let getSurveyQuestions: (id: string) => QueryBuilder;
+// Boy this one was a bit of a stretch
+let filterByField: (
+  field: string,
+  fieldValue: string,
+) => (query: QueryBuilder) => QueryBuilder;
 
+/* Don't know why but I had to protect this in a function before it would work
+   right otherwise it was returning a different sql statement every run */
 
+const baseQuery = () => db('surveys');
 
+filterByField = (field, fieldValue) => {
+  return (query) => {
+    return query.where(field, '=', fieldValue);
+  };
+};
 
+getSurvey = (id) => {
+  const filteredById = filterByField('id', id);
+  return filteredById(baseQuery());
+};
 
+getAllSurveys = () => {
+  return baseQuery();
+};
+
+getSurveyQuestions = (id) => {
+  return baseQuery()
+    .join('questions', 'questions.survey_id', '=', 'survey.id')
+    .select(
+      'survey.name',
+      'question.name',
+      'survey.isGuest',
+      'question.isGuest',
+    )
+    .where({ survey_id: id });
+};
+
+export { getSurvey, getAllSurveys, getSurveyQuestions };
