f94d3cda267041a8140ba89f46e791c7ab268b2e
BrandCam <BrandCam>
2019-02-21 14:51:39 -0600

    added template for frontend pages that need added

---
+++ frontend/src/pages/SurveyAnswers/SurveyAnswers.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Surveyanswers = () =>{
+    return(
+        <>
+        <h1>Answer surveys here!!</h1>
+        </>
+    )
+}
+
+export default Surveyanswers
---
+++ frontend/src/pages/SurveyView/SurveyView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Surveyview = () =>{
+    return(
+        <>
+        <h1>View A specific Survey Here!</h1>
+        </>
+    )
+}
+
+export default Surveyview
