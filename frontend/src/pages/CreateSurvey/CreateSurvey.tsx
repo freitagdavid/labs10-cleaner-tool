a594406ed68e9ee80dc6ed697ad3448f67cbac82
brooks <bpoltl1@gmail.com>
2019-02-25 16:38:17 -0600

    setup handlers

--- frontend/src/pages/CreateSurvey/CreateSurvey.tsx
+++ frontend/src/pages/CreateSurvey/CreateSurvey.tsx
@@ -1,30 +1,36 @@
 import React, {useState} from 'react';
 import SurveyQuestion from './SurveyQuestion'
 const CreateSurvey = ()=>{
-
-    const [dropdown, setDropdown] = useState(1)
+    const [dropdown, setDropdown] = useState(3)
+    const [surveyName, setSurveyName] = useState('')
     const handleDropdown = (ev: any) =>{
         setDropdown(ev.target.value)
+    }
+    const handleSurveyName = (ev:any) =>{
+        setSurveyName(ev.target.value)
     }
     const questionLength = () =>{
         let survey = [];
         for(let i = 1; i<=dropdown; i++){
-            survey.push(<SurveyQuestion questionNumber = {i}/>)
+            survey.push(<SurveyQuestion key = {i} questionNumber = {i}/>)
         }
         return survey
+    }
+    const submitHandler = () => {
+
     }
         return (
             <div>
                 <form>
                     <h1>Create a Survey</h1>
                     <h2>Survey Name</h2>
-                    <input type='text' placeholder='Survey Name' />
+                    <input type='text' placeholder='Survey Name' onChange = {handleSurveyName}/>
                     <div>
                         <h3>Survey Type:</h3>
-                        <button>Guest Survey</button>
-                        <button>Assitant Survey</button>
+                        <button type = 'button'>Guest Survey</button>
+                        <button type = 'button'>Assitant Survey</button>
                         <h3>How Many Questions:</h3>
-                        <select onChange={handleDropdown}>
+                        <select defaultValue = '3' onChange={handleDropdown}>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
--- frontend/src/pages/CreateSurvey/SurveyQuestion.tsx
+++ frontend/src/pages/CreateSurvey/SurveyQuestion.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-const SurveyQuestion = (questionNumber: any) => {
+const SurveyQuestion = (props: any) => {
+    const [questionType, setQuestionType] = useState(1)
+    const [question, setQuestion] = useState('')
+    const handleQuestion = (ev:any) =>{
+        setQuestion(ev.target.value)
+    }
     return (
         <div>
-            <h2>Question {}</h2>
-            <input type = 'text' placeholder = 'Add question text here'/>
+            <h2>Question {props.questionNumber}</h2>
+            <input type = 'text' placeholder = 'Add question text here' onChange = {handleQuestion}/>
             <div>
-                <button>Yes/No</button>
-                <button>1-5 Rating</button>
-                <button>Free Text</button>
+                <button type = 'button' onClick = {()=> setQuestionType(1)}>Yes/No</button>
+                <button type='button' onClick={()=>setQuestionType(2)}>1-5 Rating</button>
+                <button type='button' onClick={()=>setQuestionType(3)}>Free Text</button>
             </div>
         </div>
     )
