ff41528a80cf5a65dfa604064f5037f706122c4d
David Freitag <freitagdavid@outlook.com>
2019-02-26 17:55:15 -0500

    Got the progress bar and guest info displaying on guest dashboard

--- frontend/src/pages/GuestDashboard/GuestDashboard.tsx
+++ frontend/src/pages/GuestDashboard/GuestDashboard.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import GuestInfo from './GuestInfo';
+import useFetch from '../../helpers/useFetch';
+import GuestProgressBar from './GuestProgressBar';
+import MiscInfo from './MiscInfo';
 
-const GuestDashboard = () =>{
-    return(
-        <>
-        <h1>This is where Guests will be able to view the progress on their booking</h1>
-        </>
-    )
-}
+const GuestDashboard = () => {
+  const [fetchData, fetchErr, fetchLoading] = useFetch(
+    'https://randomuser.me/api',
+    true,
+    'get',
+  );
+  if (fetchLoading === true) {
+    return <h1>Loading</h1>;
+  }
+  {
+    const user = fetchData.results[0];
+    console.log(user);
+    return (
+      <>
+        <GuestInfo
+          name={`${user.name.first} ${user.name.last}`}
+          picture={user.picture.large}
+          houseLink='http://example.com'
+          houseName='whatever'
+          checkIn='1/27'
+          checkOut='1/28'
+        />
+        <GuestProgressBar previousCheckout={true} currentProgress={50} />
+        <MiscInfo />
+      </>
+    );
+  }
+};
 
 export default GuestDashboard;
---
+++ frontend/src/pages/GuestDashboard/MiscInfo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MiscInfo = () => {
+  return <h1>This is gonna be the bottom misc info</h1>;
+};
+
+export default MiscInfo;
