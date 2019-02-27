bad1d0f8514216d390ba53e0e6a8b54b1c275146
brooks <bpoltl1@gmail.com>
2019-02-27 10:21:31 -0600

    working on posting data

--- frontend/src/pages/CreateSurvey/CreateSurvey.tsx
+++ frontend/src/pages/CreateSurvey/CreateSurvey.tsx
@@ -1,13 +1,33 @@
 import React, {useState} from 'react';
 import SurveyQuestion from './SurveyQuestion'
-
+import { useFetch, axiosFetch }  from '../../helpers/'
 const CreateSurvey = ()=>{
     const [dropdown, setDropdown] = useState(3)
     const [surveyName, setSurveyName] = useState('')
     const [isGuest, setIsGuest] = useState(true)
     const url = process.env.REACT_APP_backendURL || 'https://labs10-cleaner-app-2.herokuapp.com';
+
+ async function handleSubmit(surveyName:string, isGuest:boolean){
+        const [fetch, setFetch] = useState(false)
+        const body = {
+            name: surveyName,
+            isGuest: isGuest,
+        }
+     await axiosFetch('post',`${url}/surveys`,body)
+     .catch((e: any) => {
+         console.error(e);
+     });
+     setFetch(!fetch)
+}
     const handleDropdown = (ev: any) =>{
         setDropdown(ev.target.value)
+    }
+    const handleGuest = (ev: any) =>{
+        if(ev.target.name === 'guest'){
+            setIsGuest(true)
+        } else {
+            setIsGuest(false)
+        }
     }
     const handleSurveyName = (ev:any) =>{
         setSurveyName(ev.target.value)
@@ -19,17 +39,17 @@ const CreateSurvey = ()=>{
         }
         return survey
     }
-
+    
         return (
             <div>
-                <form>
+                <form >
                     <h1>Create a Survey</h1>
                     <h2>Survey Name</h2>
                     <input type='text' placeholder='Survey Name' onChange = {handleSurveyName}/>
                     <div>
                         <h3>Survey Type:</h3>
-                        <button type = 'button'>Guest Survey</button>
-                        <button type = 'button'>Assitant Survey</button>
+                        <button type = 'button' name = 'guest' onClick = {handleGuest}>Guest Survey</button>
+                        <button type = 'button' name = 'assistant' onClick = {handleGuest}>Assitant Survey</button>
                         <h3>How Many Questions:</h3>
                         <select defaultValue = '3' onChange={handleDropdown}>
                             <option value="1">1</option>
@@ -41,7 +61,7 @@ const CreateSurvey = ()=>{
                         {questionLength()}
                     </div>
                     <div>
-                        <button>Save</button>
+                        <button type = 'button' onClick = {()=> handleSubmit(surveyName, isGuest)}>Save</button>
                         <button>Cancel</button>
                     </div>
                 </form>
