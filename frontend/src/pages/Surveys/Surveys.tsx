50bd1bd49cea1cbdf9351d7a6fd76e059e0da1ad
BrandCam <BrandCam>
2019-02-26 19:07:10 -0600

    added surveys page to front end

--- frontend/src/pages/Surveys/Surveys.tsx
+++ frontend/src/pages/Surveys/Surveys.tsx
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useFetch } from '../../helpers';
-import { Survey } from './types';
+import { Survey, FilterArgs } from './types';
 import loadingIndicator from '../utils/loading.svg';
-
+import { Button, Container } from '../../components/index';
+import { Link } from 'react-router-dom';
+import SurveyCard from './SurveyCard'
+import './Surveys.css'
 
 const Surveys = () => {
     const url =
     process.env.REACT_APP_backendURL || 'https://labs10-cleaner-app-2.herokuapp.com';
+    const [active, setActive] = useState(true as FilterArgs);
     const [data, err, loading] = useFetch(`${url}/surveys`)
 
-    console.log(data)
+    
+    const activeClass = (filter: FilterArgs) =>
+    active === filter ? 'active' : '';
 
+    
     return (
-        <>
-            <h1> Surveys page </h1>
+        <Container>
+            <div className="surveys-header">
+                <h1> Surveys page </h1>
+                <Link to="/createsurvey">
+                    <Button text="+ New Survey" color='var(--color-accent)'></Button>
+                </Link>
+            </div>
+            <div className='surveys-filter-buttons'>
+            <Button
+          className={`button-filter guest ${activeClass(true)}`}
+          text='Guest'
+          color='var(--color-text-accent)'
+          onClick={() => setActive(true)}
+          datatestid='button-guest'
+        />
+        <Button
+          className={`button-filter assistant ${activeClass(false)}`}
+          text='Assistant'
+          color='var(--color-text-accent)'
+          onClick={() => setActive(false)}
+          datatestid='button-assistant'
+        />
+        </div>
             <div>
                 {loading ? (
                     <img src={loadingIndicator} alt='animated loading indicator' />
                 ) : data ? (
                 data.map((survey: Survey) => 
-                    (<div className='survey' key={survey.id}>{survey.name}</div> 
+                    (
+                    <div className={`survey${activeClass(survey.isGuest)}`}>
+                        <SurveyCard {...survey} key={survey.id}></SurveyCard> 
+                    </div>
                 ))
             ) : null}
             </div>
-        </>
+        </Container>
     )
 }
 
--- frontend/src/pages/Surveys/types.ts
+++ frontend/src/pages/Surveys/types.ts
@@ -4,4 +4,7 @@ export interface Survey {
     survey: any,
     id: number,
     name: string,
+    isGuest: boolean,
 }
+
+export type FilterArgs =  true | false
