e0ecfbfa514962286086d96c7a3ce8fb6ef7fe1c
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-03-01 13:23:04 -0600

    styling on create survey

--- frontend/src/pages/CreateSurvey/CreateSurvey.styling.tsx
+++ frontend/src/pages/CreateSurvey/CreateSurvey.styling.tsx
@@ -4,24 +4,46 @@ export const CreateSurveysWrapper = styled.div`
     text-align: left;
     margin: 0 8rem 0 15rem;
     border: 1px solid red;
-    @media (max-width: 780px) {
-        margin: 0 auto;
-        width: 90%;
-        
+    @media only screen and (max-width: 400px) {
+        margin: 0 auto; 
         border: 1px solid blue;
     }
 `;
 
+export const CreateSurveyOptions = styled.div`
+    background-color: white;
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+    border: 1px solid #D8D8D8;
+    border-radius: 5px;
+`;
+
+export const CreateSurveyHeader = styled.h1`
+    font-size: 2rem;
+    font-weight: normal;
+    border: 1px solid green;
+    @media only screen and (max-width: 400px) {
+        text-align: center;
+        
+        margin-top: 2rem;
+        border: 1px solid purple;
+    }
+`;
+
+
 export const CreateSurveyLables = styled.h2`
     font-size: .9rem;
+    color: var(--color-text-accent);
     //border: 1px solid green;
+    @media only screen and (max-width: 400px) {
+        border: 1px solid red;
+    }
 `;
 
 export const CreateSurveyInput = styled.input`
     padding-left: .5rem;
     width: 70%;
     height: 30px;
-    border: 1.5px solid #D8D8D8;
+    border-bottom: 1.5px solid #D8D8D8;
     @media (max-width: 780px) {
         width: 100%;
     }
@@ -43,7 +65,10 @@ export const SurveyType = styled.h3`
 `;
 
 export const SurveyTypeButton = styled.button`
-    box-shadow: .2rem .2rem #6D6E71;
+    background-color: #428ACB;
+    color: white;
+    margin-right: .5rem;
+    //box-shadow: .2rem .2rem #6D6E71;
     height: 25px;
 `;
 
@@ -53,6 +78,7 @@ export const SurveyQuestions = styled.h3`
 `;
 
 export const CreateSurveyQuestionWrapper = styled.div`
+    background-color: white;
     margin-top: 1rem;
     border: 1px solid purple;
 `;
