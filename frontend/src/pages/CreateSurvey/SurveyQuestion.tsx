440aa1b624a3c1435742fdbe357a782661965f73
BrandCam <BrandCam>
2019-03-05 12:53:12 -0600

    added active classes to buttons on create survey page

--- frontend/src/pages/CreateSurvey/SurveyQuestion.tsx
+++ frontend/src/pages/CreateSurvey/SurveyQuestion.tsx
@@ -8,9 +8,9 @@ const SurveyQuestion = (props: any) => {
             <CreateSurveyQuestionLables>Question {props.questionNumber}</CreateSurveyQuestionLables>
             <CreateSurveyQuestionInput type = 'text' placeholder = 'Add question text here' onChange = {(ev:any)=>{props.setQuestion(ev.target.value)}}/>
             <div>
-                <button type='button' onClick={() => props.setQuestionType(1)}>Yes/No</button>
-                <button type='button' onClick={() => props.setQuestionType(2)}>1-5 Rating</button>
-                <button type='button' onClick={() => props.setQuestionType(3)}>Free Text</button>
+                <button className = {`${props.activeClass(1, props.questionType)}`} type='button' onClick={() => props.setQuestionType(1)}>Yes/No</button>
+                <button className = {`${props.activeClass(2, props.questionType)}`} type='button' onClick={() => props.setQuestionType(2)}>1-5 Rating</button>
+                <button className = {`${props.activeClass(3, props.questionType)}`} type='button' onClick={() => props.setQuestionType(3)}>Free Text</button>
             </div>
         </CreateSurveyQuestionWrapper>
     )
