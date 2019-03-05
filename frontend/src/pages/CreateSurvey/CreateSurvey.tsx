6df6171afbf24de9bbd55abeeccf9daaf489a824
brooks <bpoltl1@gmail.com>
2019-03-05 10:32:22 -0600

    fixed merge issue

--- frontend/src/pages/CreateSurvey/CreateSurvey.tsx
+++ frontend/src/pages/CreateSurvey/CreateSurvey.tsx
@@ -126,8 +126,8 @@ const CreateSurvey = (props: any)=>{
                         <CreateSurveyInput type='text' placeholder='Survey Name' onChange={(event: any) => { setSurveyName(event.target.value) }}/>
                         <SurveyOptions>
                             <SurveyType>Survey Type:</SurveyType>
-                            <SurveyTypeButton type = 'button'>Guest Survey</SurveyTypeButton>
-                            <SurveyTypeButton type = 'button'>Assitant Survey</SurveyTypeButton>
+                            <SurveyTypeButton type = 'button' onClick = {()=> setIsGuest(true)}>Guest Survey</SurveyTypeButton>
+                            <SurveyTypeButton type = 'button' onClick = {()=> setIsGuest(false)}>Assitant Survey</SurveyTypeButton>
                             <SurveyQuestions>How Many Questions:</SurveyQuestions>
                             <select defaultValue = '3' onChange={(event: any) => {
                                 event.preventDefault()
