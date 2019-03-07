c4466c184a59f817820a83a30dce6d30fe310baa
David Freitag <freitagdavid@outlook.com>
2019-03-06 19:58:59 -0500

    frontend/src/pages/SurveyResponse/Response: Added interface

--- frontend/src/pages/SurveyResponse/Response.tsx
+++ frontend/src/pages/SurveyResponse/Response.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Response = (props:any) => {
-    return(
-        <div className="single-response">
-            <h3>Question{props.qid}</h3>
-            <h4>{props.question}</h4>
-            <h5>{props.answer}</h5>
-        </div>
-    )
+interface ResponseProps {
+  qid: number;
+  question: string;
+  answer: string;
 }
+
+const Response = (props: ResponseProps) => {
+  return (
+    <div className='single-response'>
+      <h3>Question{props.qid}</h3>
+      <h4>{props.question}</h4>
+      <h5>{props.answer}</h5>
+    </div>
+  );
+};
 
 export default Response;
