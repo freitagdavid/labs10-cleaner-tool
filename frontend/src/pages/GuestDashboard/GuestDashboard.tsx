c8eb57eec4c4d23c5629c99dc781bb3367266960
David Freitag <freitagdavid@outlook.com>
2019-03-11 20:31:48 -0400

    frontend/src/pages/GuestDashboard/GuestDashboard: Stripped useless code

--- frontend/src/pages/GuestDashboard/GuestDashboard.tsx
+++ frontend/src/pages/GuestDashboard/GuestDashboard.tsx
@@ -3,33 +3,10 @@ import GuestInfo from './GuestInfo';
 import useFetch from '../../helpers/useFetch';
 import GuestProgressBar from './GuestProgressBar';
 import MiscInfo from './MiscInfo';
-import { StyledGuestDashboard } from './GuestDash.styling';
+import { StyledGuestDashboard } from './GuestDashboard.styling';
 import defaultUser from '../../assets/default-user.jpg';
 
 const backendURL = process.env.REACT_APP_backendURL;
-
-const timeObject = (dateTime: string) => {
-  const re = /(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d)\:(\d\d)\:(\d\d)\.(\d\d\d)Z/i;
-  // Run regex on the passed in dateTime to split it into seperate date/time items
-  const date = dateTime.match(re);
-  // Convert the data to integers to make TS shutup
-  let integerified = new Array<number>(8);
-  integerified = date!.map((i) => parseInt(i, 10));
-  // Remove first result since it's a duplicate of the second
-  let cruftRemoved = Array<number>(7);
-  cruftRemoved = [...integerified].slice(1, 7);
-  // @ts-ignore
-  return new Date(...cruftRemoved);
-};
-
-const formatDate = (dateTime: Date) => {
-  return `${dateTime.getDay()}/${dateTime.getMonth()}/${dateTime.getFullYear()}`;
-};
-
-const handleDate = (dateTime: string) => {
-  const dateObj = timeObject(dateTime);
-  return formatDate(dateObj);
-};
 
 const GuestDashboard = (props: any) => {
   console.log(props.match);
@@ -49,8 +26,6 @@ const GuestDashboard = (props: any) => {
       </div>
     );
   } else {
-    console.log(timeObject(fetchData.check_in));
-
     return (
       <StyledGuestDashboard>
         <GuestInfo
@@ -62,9 +37,11 @@ const GuestDashboard = (props: any) => {
           checkOut={fetchData.check_out}
           houseAddress={fetchData.house_address}
         />
-
-        <GuestProgressBar tasks={fetchData.checklist} />
-        <MiscInfo id={props.match.params.id} />
+        <section>
+          <h2>Stay Information and Progress</h2>
+          <GuestProgressBar tasks={fetchData.checklist} />
+          <MiscInfo id={props.match.params.id} />
+        </section>
       </StyledGuestDashboard>
     );
   }
