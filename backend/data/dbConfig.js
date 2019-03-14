f72add2adbe2c9800ac9802369a55e2d4ae8a20b
BrandCam <BrandCam>
2019-03-14 18:18:35 -0500

    uncomment

--- backend/data/dbConfig.js
+++ backend/data/dbConfig.js
@@ -5,9 +5,9 @@ const knexConfig = require('../knexfile');
 // default to 'development' if no .env
 const environment = process.env.NODE_ENV || 'production';
 
-// const db = knex(knexConfig[environment]);
-// db.migrate.rollback();
-// db.migrate.latest();
-// db.seed.run();
+const db = knex(knexConfig[environment]);
+db.migrate.rollback();
+db.migrate.latest();
+db.seed.run();
 
 module.exports = db;
