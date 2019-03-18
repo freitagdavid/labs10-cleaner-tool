43e8924180fd24b52da641cca3a33318f293df66
David Freitag <freitagdavid@outlook.com>
2019-03-18 19:24:25 -0400

    frontend/src/pages/Surveys/types: Another part of that fix

--- frontend/src/pages/Surveys/types.ts
+++ frontend/src/pages/Surveys/types.ts
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export interface Survey {
-    survey: any,
-    id: number,
-    name: string,
-    isGuest: boolean,
+  survey: any,
+  id: number,
+  name: string,
+  isGuest: boolean,
 }
 
-export type FilterArgs =  boolean
+export type FilterArgs = boolean | number;
