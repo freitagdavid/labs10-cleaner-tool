0fa57da09f2a68cfe58c437725e122d05d714405
brooks <bpoltl1@gmail.com>
2019-03-18 11:12:36 -0500

    survey fix

--- backend/data/dbConfig.js
+++ backend/data/dbConfig.js
@@ -6,8 +6,8 @@ const knexConfig = require('../knexfile');
 const environment = process.env.NODE_ENV || 'production';
 
 const db = knex(knexConfig[environment]);
-db.migrate.rollback();
-db.migrate.latest();
-db.seed.run();
+// db.migrate.rollback();
+// db.migrate.latest();
+// db.seed.run();
 
 module.exports = db;
