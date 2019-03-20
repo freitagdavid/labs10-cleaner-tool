0f494012d561986e1bee810eda675a7a3ba8e7aa
Sasha <taylork85@gmail.com>
2019-03-20 12:45:50 -0500

    boolean fix

--- frontend/src/pages/Surveys/Surveys.tsx
+++ frontend/src/pages/Surveys/Surveys.tsx
@@ -23,8 +23,8 @@ const Surveys = () => {
   const [active, setActive] = useState();
 
   useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {
-      setActive(true);
+    if (process.env.NODE_ENV !== 'development') {
+      setActive(1);
     }
   }, [])
 
