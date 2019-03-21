cf55815bf541ec0af7d2632c2b06fe5e99290c05
Sasha <taylork85@gmail.com>
2019-03-21 11:48:58 -0500

    date fix

--- backend/data/migrations/20190218160153_questionAnswers.js
+++ backend/data/migrations/20190218160153_questionAnswers.js
@@ -20,7 +20,7 @@ exports.up = function(knex, Promise) {
     table.string('photo');
     table.string('house_name');
     table
-    .date('created_at');
+    .date('created_at').defaultTo(knex.fn.now());
   });
 };
 
